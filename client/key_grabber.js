739e5defd4055e6f7e002c418a4f5a0fa3e9b522
shyndman <scotty.hyndman@gmail.com>
2011-03-18 19:19:20 -0400

    * key grabbing content script created for shortcuts * background can now watches for requests from the content script

---
+++ client/key_grabber.js
@@ -0,0 +1,24 @@
+function consumeKeydown(e) {
+  var target = e.target;
+  if (target.tagName == 'TEXTAREA' ||
+      target.tagName == 'SELECT' ||
+      (target.tagName == 'INPUT' &&
+        (target.type == '' || target.type.toUpperCase() == 'TEXT' ||
+          target.type.toUpperCase() == 'PASSWORD' ||
+          target.type.toUpperCase() == 'SEARCH'))) {
+    return;
+  }
+  
+  console.dir(e);
+  
+  if ((!e.metaKey && !e.ctrlKey) || !e.shiftKey || e.keyCode != 80) {
+    return;
+  }
+  
+  // Send a request to the background page
+  chrome.extension.sendRequest({cmd: "poke"}, function(response) {
+    console.log(response);
+  });
+}
+
+window.addEventListener('keydown', consumeKeydown, true);
